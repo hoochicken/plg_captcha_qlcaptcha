b2367add1d102b69d0ca0a62594aef0de0b2f8bd
hoochicken <mareike.riegel@ql.de>
2022-10-08 12:59:28 +0200

    feat (code) code conventions

--- php/classes/plgCaptchaQlcaptchaCalculator.php
+++ php/classes/plgCaptchaQlcaptchaCalculator.php
@@ -19,29 +19,22 @@ class plgCaptchaQlcaptchaCalculator extends plgCaptchaQlcaptchaSimplex
      */
     function randomText()
     {
-        $this->level=1;
-        $first=rand(5,20);
-        $second=rand(5,10);
-        $type=array_rand(array(0,1));
-        if(0==$type AND $first>$second AND 0==$first%$second)
-        {
-            $string=$first.' : '.$second;
-            $this->solution=$first/$second;
-        }
-        elseif(0==$type AND $first>$second)
-        {
-            $this->solution=$first-$second;
-            $string=$first.' - '.$second;
-        }
-        elseif(1==$type AND $first<=10 AND $second<=10)
-        {
-            $string=$first.' x '.$second;
-            $this->solution=$first*$second;
-        }
-        else
-        {
-            $string=$first.' + '.$second;
-            $this->solution=$first+$second;
+        $this->level = 1;
+        $first = rand(5, 20);
+        $second = rand(5, 10);
+        $type = array_rand(array(0, 1));
+        if (0 == $type and $first > $second and 0 == $first % $second) {
+            $string = $first . ' : ' . $second;
+            $this->solution = $first / $second;
+        } elseif (0 == $type and $first > $second) {
+            $this->solution = $first - $second;
+            $string = $first . ' - ' . $second;
+        } elseif (1 == $type and $first <= 10 and $second <= 10) {
+            $string = $first . ' x ' . $second;
+            $this->solution = $first * $second;
+        } else {
+            $string = $first . ' + ' . $second;
+            $this->solution = $first + $second;
         }
         return $string;
     }
--- qlcaptcha.php
+++ qlcaptcha.php
@@ -15,49 +15,52 @@ defined('_JEXEC') or die;
  */
 class PlgCaptchaQlcaptcha extends JPlugin
 {
-	/**
-	 * Load the language file on instantiation.
-	 *
-	 * @var    boolean
-	 * @since  3.1
-	 */
-	protected $autoloadLanguage = true;
-	/**
+    /**
+     * Load the language file on instantiation.
+     *
+     * @var    boolean
+     * @since  3.1
+     */
+    protected $autoloadLanguage = true;
+
+    /**
      * constructor
      *setting language
      */
-    public function __construct(& $subject, $config)
+    public function __construct(&$subject, $config)
     {
-		parent::__construct($subject, $config);
+        parent::__construct($subject, $config);
         $this->loadLanguage();
     }
-	/**
-	 * Initialise the captcha
-	 *
-	 * @param   string  $id  The id of the field.
-	 *
-	 * @return  Boolean	True on success, false otherwise
-	 *
-	 * @throws  Exception
-	 *
-	 * @since  2.5
-	 */
-	public function onInit($id = 'qlcaptcha')
-	{
-        require_once(JPATH_ROOT.'/plugins/captcha/qlcaptcha/php/classes/plgCaptchaQlcaptcha2.php');
-        $tmp='tmp/'.$this->get('_name');
-        $this->obj_captcha=new plgCaptchaQlcaptcha2();
+
+    /**
+     * Initialise the captcha
+     *
+     * @param string $id The id of the field.
+     *
+     * @return  Boolean    True on success, false otherwise
+     *
+     * @throws  Exception
+     *
+     * @since  2.5
+     */
+    public function onInit($id = 'qlcaptcha')
+    {
+        require_once(JPATH_ROOT . '/plugins/captcha/qlcaptcha/php/classes/plgCaptchaQlcaptcha2.php');
+        $tmp = 'tmp/' . $this->get('_name');
+        $this->obj_captcha = new plgCaptchaQlcaptcha2();
         $this->obj_captcha->checkTmpQlcaptcha($tmp);
-        $objPlg=clone $this;
-        $objPlg->extName=$this->get('_name');
-        $this->obj_captcha->initiateCaptcha($objPlg,$tmp);
+        $objPlg = clone $this;
+        $objPlg->extName = $this->get('_name');
+        $this->obj_captcha->initiateCaptcha($objPlg, $tmp);
     }
+
     /**
      * Gets the challenge HTML
      *
-     * @param   string  $name   The name of the field. Not Used.
-     * @param   string  $id     The id of the field.
-     * @param   string  $class  The class of the field. This should be passed as
+     * @param string $name The name of the field. Not Used.
+     * @param string $id The id of the field.
+     * @param string $class The class of the field. This should be passed as
      *                          e.g. 'class="required"'.
      *
      * @return  string  The HTML to be embedded in the form.
@@ -66,35 +69,36 @@ class PlgCaptchaQlcaptcha extends JPlugin
      */
     public function onDisplay($name = null, $id = 'dynamic_qlcaptcha_1', $class = '')
     {
-        $html=array();
-        $html[]='<div class="image">';
-        $html[]='<img src="'.$this->obj_captcha->captcha.'"/>';
-        $html[]='</div>';
-        $html[]='<div class="input">';
-        $html[]='<input ';
-        if(1==$this->params->get('captchaLabelswithin')) $html[]='placeholder="'.JText::_($this->params->get('captchadesc')).'" ' ;
-        $html[]='type="text" autocorrect="off" autocapitalize="off" name="qlcaptcha" id="qlcaptcha" value="" />';
-        $html[]='<input type="hidden" name="qlcaptchakey" value="'.$this->obj_captcha->key.'" />';
-        $html[]='</div>';
-        $html[]='<div class="description">';
-        if(1!=$this->params->get('captchaLabelswithin',0))$html[]='<label for="captcha"><span>'.JText::_($this->params->get('captchadesc')).'</span></label>';
-        $html[]='</div>';
-		return implode("\n",$html);
+        $html = array();
+        $html[] = '<div class="image">';
+        $html[] = '<img src="' . $this->obj_captcha->captcha . '"/>';
+        $html[] = '</div>';
+        $html[] = '<div class="input">';
+        $html[] = '<input ';
+        if (1 == $this->params->get('captchaLabelswithin')) $html[] = 'placeholder="' . JText::_($this->params->get('captchadesc')) . '" ';
+        $html[] = 'type="text" autocorrect="off" autocapitalize="off" name="qlcaptcha" id="qlcaptcha" value="" />';
+        $html[] = '<input type="hidden" name="qlcaptchakey" value="' . $this->obj_captcha->key . '" />';
+        $html[] = '</div>';
+        $html[] = '<div class="description">';
+        if (1 != $this->params->get('captchaLabelswithin', 0)) $html[] = '<label for="captcha"><span>' . JText::_($this->params->get('captchadesc')) . '</span></label>';
+        $html[] = '</div>';
+        return implode("\n", $html);
     }
+
     /**
      * Calls an HTTP POST function to verify if the user's guess was correct
-     * @param   string  $code  Answer provided by user. Not needed for the Qlcaptcha implementation
+     * @param string $code Answer provided by user. Not needed for the Qlcaptcha implementation
      * @return  True if the answer is correct, false otherwise
      * @since  2.5
      */
     public function onCheckAnswer($code = null)
     {
-        $input=JFactory::getApplication()->input;
-        $strCaptcha=$input->getString('qlcaptcha');
-        $key=$input->getString('qlcaptchakey');
-        require_once(JPATH_ROOT.'/plugins/captcha/qlcaptcha/php/classes/plgCaptchaQlcaptcha2.php');
-        $validated=plgCaptchaQlcaptcha2::checkCaptcha($strCaptcha,$key,true);
-        if (false==$validated) $this->_subject->setError(JText::_('PLG_CAPTCHA_QLCAPTCHA_MSG_CAPTCHAFAILED'));
+        $input = JFactory::getApplication()->input;
+        $strCaptcha = $input->getString('qlcaptcha');
+        $key = $input->getString('qlcaptchakey');
+        require_once(JPATH_ROOT . '/plugins/captcha/qlcaptcha/php/classes/plgCaptchaQlcaptcha2.php');
+        $validated = plgCaptchaQlcaptcha2::checkCaptcha($strCaptcha, $key, true);
+        if (false == $validated) $this->_subject->setError(JText::_('PLG_CAPTCHA_QLCAPTCHA_MSG_CAPTCHAFAILED'));
         return $validated;
     }
 }
