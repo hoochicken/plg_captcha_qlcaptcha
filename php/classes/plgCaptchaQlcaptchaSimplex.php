c3e6a798a560706f584685ba76ff60d439454f49
hoochicken <mareike.riegel@ql.de>
2022-10-08 13:03:12 +0200

    code (manifest) year adjusted

--- php/classes/plgCaptchaQlcaptchaSimplex.php
+++ php/classes/plgCaptchaQlcaptchaSimplex.php
@@ -2,7 +2,7 @@
 /**
  * @package     plg_captcha_qlcaptcha
  * @subpackage  Captcha
- * @copyright   Copyright (C) 2016 ql.de. All rights reserved.
+ * @copyright   Copyright (C) 2022 ql.de. All rights reserved.
  * @author      Mareike Riegel
  * @email       mareike.riegel@ql.de
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
